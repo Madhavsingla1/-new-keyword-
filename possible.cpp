123f6d076dda0212e183fc9ee2976f5ccc11426b
pranav1812 <pranavvohra1812@yahoo.in>
2020-01-29 11:35:45 +0530

    checking whether delete operator deletes the pointer or not

---
+++ possible.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int **q=new int*;
+    int *p=new int;
+    cin>>*p;
+    *q=p;
+    cout<<*q<<endl;
+
+    cout<<*p<<endl;
+    delete p;
+    cout<<*p<<endl;
+    // only value at the dynamically alloted pointer is deleted
+    // pointer is still there
+    cout<<*q<<endl;
+    return 0;
+}
