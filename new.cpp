ffbbbd319a21e525454becc0bce652e60ff794c0
pranav1812 <root@localhost.localdomain>
2020-01-29 11:01:10 +0530

    created a pointer to pointer

--- new.cpp
+++ new.cpp
@@ -2,8 +2,12 @@
 using namespace std;
 
 int main(){
+    int **q=new int*;
     int *p=new int;
     cin>>*p;
+    *q=p;
+    cout<<*q<<endl;
+
     cout<<*p<<endl;
     delete p;
     cout<<*p<<endl;
